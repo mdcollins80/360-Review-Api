5985f9951c0cc9dc81e4437c565469ae472cc361
mdcollins80 <mdcollins80@gmail.com>
2017-11-03 14:30:13 -0400

    Adds admin boolean to users table
    - `bin/rails generate migration add_admin_to_users admin:boolean` used
      to add admin boolean to users table.
    - Updates user serializer to include names, business unit, and admin
      status.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :given_name, :surname, :business_unit,
+             :admin
 end
